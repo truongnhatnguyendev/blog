858b2a8da6d3d7b684ecb419fe7bc71d4f2418da
truongnhatnguyen <truongnhatnguyen9999@gmail.com>
2025-01-08 16:34:24 +0700

    thêm thư viện morgan

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
 const port = 3000;
+
+app.use(morgan("combined"));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
